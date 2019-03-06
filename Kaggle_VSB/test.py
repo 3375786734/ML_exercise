cb2bdad1706279d2c8b2ef3d1fa0acd128a568b5
li1113 <www.375786734@qq.com>
2019-03-06 22:58:37 +0800

    edit

--- Kaggle_VSB/test.py
+++ Kaggle_VSB/test.py
@@ -1,6 +1,7 @@
 import IO_interface.IOPY as iopy
 import seq_feature
 import numpy as np
+import lightgbm as lgb
 oi = iopy.io_py()
 data_i,data_t = "metadata_train.csv","sample.csv"
 datai,datat = oi.read_data(data_i,"pd"),oi.read_data(data_t,"pd")
@@ -18,8 +19,11 @@ datai = datai.set_index(['id_measurement','phase'])
 #这里取了index中levels[0]不同的即,一共有8712个数据,其中 8712/3 根不同线路,因此只有8712/3个index
 print(np.array(datai.index.levels[0].unique()).shape)
 '''
+'''
 ext = seq_feature.feature_extracter(datat,datai)
 X,y = ext.get_feature_1()
 print("X shape",X.shape)
 print("y shape",y.shape)
 oi.write_data('feature.csv',X,'csv')
+'''
+
--- Kaggle_VSB/ttt.py
+++ Kaggle_VSB/ttt.py
@@ -2,3 +2,6 @@ import numpy as np
 a = [[[1,2,3],[4,5,6]],[[7,8,9],[11,12,13]]]
 print(np.array(a).shape)
 print(np.array(np.concatenate(a,axis=1)))
+print("here")
+b = [1,2,3,4,5,6,7,8,9,10]
+print(np.clip(b,np.percentile(b,1),np.percentile(b,80)))
