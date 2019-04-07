dcd502023f42fe065cd60652f070a87b69554aae
li1113 <www.375786734@qq.com>
2019-04-07 09:42:06 +0800

    edit

--- CS231N/assignment2_v2/cs231n/layers.py
+++ CS231N/assignment2_v2/cs231n/layers.py
@@ -25,9 +25,9 @@ def affine_forward(x, w, b):
     # TODO: Implement the affine forward pass. Store the result in out. You   #
     # will need to reshape the input into rows.                               #
     ###########################################################################
+    x_reshape = x.reshape(x.shape[0],-1)
+    out = x_reshape.dot(w)+b
     cache = (x, w, b)
-    x = x.reshape(x.shape[0],-1)
-    out = x.dot(w)+b
     return out, cache
 
 
@@ -53,9 +53,9 @@ def affine_backward(dout, cache):
     ###########################################################################
     # TODO: Implement the affine backward pass.                               #
     ###########################################################################
-    dx,dw,db = w.T,(x.reshape(x.shape[0],-1)).T,np.ones((num_data,1))
+    x_reshape = x.reshape(x.shape[0],-1)
+    dx,dw,db = dout.dot(w.T),x_reshape.T.dot(dout),np.sum(dout,axis = 0)
     dx = dx.reshape(x.shape)
-
     return dx, dw, db
 
 
@@ -74,7 +74,7 @@ def relu_forward(x):
     ###########################################################################
     # TODO: Implement the ReLU forward pass.                                  #
     ###########################################################################
-    
+
     cache = x
     return out, cache
 
